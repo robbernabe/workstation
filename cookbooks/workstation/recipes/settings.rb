1b82475adc0d6479b5371ec792f9dec2fa362e61
Rob Bernabé <rbernabe@gmail.com>
2015-12-21 16:10:31 -0500

    New method for determining user to run commands as.

--- cookbooks/workstation/recipes/settings.rb
+++ cookbooks/workstation/recipes/settings.rb
@@ -6,22 +6,18 @@
 # Resources:
 #   - http://jtimberman.housepub.org/blog/2014/12/29/chef-12-homebrew-user-mixin/
 
-Chef::Resource::Execute.send(:include, Chef::Mixin::HomebrewUser)
-
-# directory "/Library/Application\ Support/GarageBand" do
-#   action :delete
-#   recursive true
-# end
-# 
-# directory "/Applications/GarageBand.app" do
-#   action :delete
-#   recursive true
-# end
+user = Mixlib::ShellOut.new("id -un ${SUDO_USER}").run_command.stdout.to_s.strip
 
 # Before getting started, make sure perms are correct on our preference file!
-execute 'Set ownership properly on GlobalPreferences file' do
-  command "chown #{find_homebrew_uid}:staff ~/Library/Preferences/.GlobalPreferences.plist"
+file "/Users/#{user}/Library/Preferences/.GlobalPreferences.plist" do
+  mode  '0600'
+  owner user
+  group 'staff'
 end
+
+# execute 'Set ownership properly on GlobalPreferences file' do
+#   command "chown #{user}:staff ~/Library/Preferences/.GlobalPreferences.plist"
+# end
 
 ###############################################################################
 # Safari                                                                      #
@@ -29,7 +25,7 @@ end
 
 execute 'Safari: Re-enable backspace/delete to go back in Safari' do
   command 'defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool YES'
-  user    find_homebrew_uid
+  user    user
 end
 
 ###############################################################################
@@ -38,7 +34,7 @@ end
 
 execute 'Apple Mail: Disable inline attachments (just show the icons)' do
   command 'defaults write com.apple.mail DisableInlineAttachmentViewing -bool true'
-  user    find_homebrew_uid
+  user    user
 end
 
 ###############################################################################
@@ -47,7 +43,7 @@ end
 
 execute 'Chrome: Disable swipe to to navigate' do
   command 'defaults write com.google.Chrome.plist AppleEnableSwipeNavigateWithScrolls -bool FALSE'
-  user    find_homebrew_uid
+  user    user
 end
 
 ###############################################################################
@@ -56,65 +52,65 @@ end
 
 execute 'Finder: Use list view in all Finder windows by default' do
   command 'defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"'
-  user    find_homebrew_uid
+  user    user
 end
 
 execute 'Finder: Show icons for external hard drives on the desktop' do
   command 'defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true'
-  user    find_homebrew_uid
+  user    user
 end
 
 execute 'Finder: Show icons for hard drives on the desktop' do
   command 'defaults write com.apple.finder ShowHardDrivesOnDesktop -bool true'
-  user    find_homebrew_uid
+  user    user
 end
 execute 'Finder: Show icons for servers on the desktop' do
   command 'defaults write com.apple.finder ShowMountedServersOnDesktop -bool true'
-  user    find_homebrew_uid
+  user    user
 end
 execute 'Finder: Show icons for removable media on the desktop' do
   command 'defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true'
-  user    find_homebrew_uid
+  user    user
 end
 
 execute 'Finder: Show path in Title Bar' do
   command 'defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES'
-  user    find_homebrew_uid
+  user    user
 end
 
 execute 'Finder: New Finder window shows $HOME' do
   command 'defaults write com.apple.finder NewWindowTarget -string "PfHm"'
-  user    find_homebrew_uid
+  user    user
 end
 
 execute 'Finder: Show the ~/Library folder' do
   command 'chflags nohidden ~/Library'
-  user    find_homebrew_uid
+  user    user
 end
 
 execute 'Finder: Show the path bar' do
   command 'defaults write com.apple.finder ShowPathBar -boolean true'
-  user    find_homebrew_uid
+  user    user
 end
 
 execute 'Finder: Set sidebar icon size to small' do
   command 'defaults write NSGlobalDomain NSTableViewDefaultSizeMode -int 1'
-  user    find_homebrew_uid
+  user    user
 end
 
 execute 'Finder: Scrollbar fix for finder and Terminal' do
   command 'defaults write -g AppleShowScrollBars -string WhenScrolling'
-  user    find_homebrew_uid
+  user    user
 end
 
 execute 'Finder: Expand save panel by default' do
   command 'defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true'
-  user    find_homebrew_uid
+  user    user
 end
 
 execute 'Finder: Expand print panel by default' do
   command 'defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true'
-  user    find_homebrew_uid
+  user    user
 end
 
 ###############################################################################
@@ -123,7 +119,7 @@ end
 
 execute 'Enable AirDrop over Ethernet' do
   command 'defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1'
-  user    find_homebrew_uid
+  user    user
 end
 
 ###############################################################################
@@ -134,7 +130,7 @@ end
 # Be sure to restart iTerm2 after running this.
 execute 'iTerm2: Fix scroll to top bug' do
   command 'defaults write com.googlecode.iterm2 NSScrollAnimationEnabled -bool NO'
-  user    find_homebrew_uid
+  user    user
 end
 
 ###############################################################################
@@ -143,17 +139,17 @@ end
 
 execute 'Power: Disable Mac from sleeping entirely' do
   command '/usr/bin/sudo /usr/bin/pmset -a sleep 0'
-  user    find_homebrew_uid
+  user    user
 end
 
 execute 'Power: Never put the hard disks to sleep' do
   command '/usr/bin/sudo /usr/bin/pmset -a disksleep 0'
-  user    find_homebrew_uid
+  user    user
 end
 
 execute 'Power: Put display to sleep after 10 minutes' do
   command '/usr/bin/sudo /usr/bin/pmset -a displaysleep 10'
-  user    find_homebrew_uid
+  user    user
 end
 
 ###############################################################################
@@ -162,12 +158,12 @@ end
 
 execute 'Mouse/Trackpad: Disable “natural” scrolling' do
   command 'defaults write ~/Library/Preferences/.GlobalPreferences.plist com.apple.swipescrolldirection -boolean NO'
-  user    find_homebrew_uid
+  user    user
 end
 
 execute 'Mouse/Trackpad: Disable mouse pointer "shake to locate"' do
   command 'defaults write ~/Library/Preferences/.GlobalPreferences CGDisableCursorLocationMagnification -bool YES'
-  user   find_homebrew_uid
+  user   user
 end
 
 ###############################################################################
@@ -176,7 +172,7 @@ end
 
 execute 'Restart Dock' do
   command 'killall Dock'
-  user    find_homebrew_uid
+  user    user
   action  :nothing
 end
 
@@ -197,6 +193,16 @@ end
 
 execute 'Hot Corners: Bottom left screen corner → Start screen saver' do
   command 'defaults write com.apple.dock wvous-bl-corner -int 5 && defaults write com.apple.dock wvous-bl-modifier -int 0'
-  user    find_homebrew_uid
+  user    user
   notifies :run, 'execute[Restart Dock]'
 end
+
+# directory "/Library/Application\ Support/GarageBand" do
+#   action :delete
+#   recursive true
+# end
+#
+# directory "/Applications/GarageBand.app" do
+#   action :delete
+#   recursive true
+# end
