98d2eb03083d885da4bfa84a95be9a31041d6545
Rob Bernabé <rbernabe@gmail.com>
2016-01-20 16:55:38 -0500

    Add insomniax cask for new KVM/Monitor setup.

--- cookbooks/workstation/attributes/default.rb
+++ cookbooks/workstation/attributes/default.rb
@@ -23,5 +23,5 @@ default[:workstation][:casks]     = %w(
   handbrake iterm2-nightly launchrocket licecap macdown slack
   mysqlworkbench screenhero skype xquartz vlc wireshark viscosity
   backblaze istat-menus sequel-pro dockertoolbox virtualbox
-  vagrant vmware-fusion evernote flux appcleaner
+  vagrant vmware-fusion evernote flux appcleaner insomniax
 )
